f575c25c9aa62cb5a765f9b46fe7d2de77c3d7cb
TheDerpGamer <garret2kool@gmail.com>
2021-07-16 17:28:29 -0700

    fuck floating point shits

--- src/thederpgamer/decor/utils/MathUtils.java
+++ src/thederpgamer/decor/utils/MathUtils.java
@@ -4,6 +4,7 @@ import javax.vecmath.Quat4f;
 import javax.vecmath.Vector3f;
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.math.RoundingMode;
 
 /**
  * Various math related utility functions.
@@ -18,7 +19,7 @@ public class MathUtils {
      * @param out The Quat to round.
      */
     public static void roundQuat(Quat4f out) {
-        MathContext context = new MathContext(0);
+        MathContext context = new MathContext(0, RoundingMode.UP);
         BigDecimal bdX = new BigDecimal(out.x);
         BigDecimal bdY = new BigDecimal(out.y);
         BigDecimal bdZ = new BigDecimal(out.z);
@@ -27,7 +28,7 @@ public class MathUtils {
     }
 
     public static void roundVector(Vector3f out) {
-        MathContext context = new MathContext(0);
+        MathContext context = new MathContext(0, RoundingMode.UP);
         BigDecimal bdX = new BigDecimal(out.x);
         BigDecimal bdY = new BigDecimal(out.y);
         BigDecimal bdZ = new BigDecimal(out.z);
